295e8bea6fa4cdae4de0935ca23052e3e25d7c12
Israel Vergara <israel.vergara102824@gmail.com>
2017-08-31 23:36:11 -0500

    se agregan comentarios

--- Casa.java
+++ Casa.java
@@ -1,69 +1,78 @@
-
 /**
  * Descripción: El programa debe realizar el armado automatico de una casa con las
  * siguientes figuras: triangulo, cuadrado y circulo.
- * Autor: Israel Vergara Navor.
- * Fecha: 17 Agosto de 2017.
+ * @author Israel Vergara Navor.
+ * @version 17 Agosto de 2017.
  * Profesor: Eloy Hernández.
  */
 
-class Casa
+public class Casa
 {
-    Circle ventana;
-    Triangle techo;
-    Square pared;
-    Square puerta;
-    Casa casa2;
-
-Casa()
-{
-    ventana = new Circle();
-    techo =  new Triangle();
-    pared = new Square();
-    puerta = new Square();
+    private Circle ventana;
+    private Triangle techo;
+    private Square pared;
+    private Square puerta;
+    private Casa casa2;
     
-}
-
-void dibujate()
-{
-    ventana.makeVisible();
-    techo.makeVisible();
-    pared.makeVisible();
-    puerta.makeVisible();
-}
-
-void acomodate(int desplazar)
-{   
-    ventana.moveHorizontal(30+desplazar);
-    ventana.moveDown();
-    ventana.moveVertical(-28);
+   /**
+    * constructor que inicializa las variables ventana, techo, pared y puerta.
+    */
+   public Casa()
+   {   
+        ventana = new Circle();
+        techo =  new Triangle();
+        pared = new Square();
+        puerta = new Square();
+   }
+   
+   /**
+    * Metodo que dibuja figuras en la terminal grafica
+    */
+   public void dibujate()
+   {
+        ventana.makeVisible();
+        techo.makeVisible();
+        pared.makeVisible();
+        puerta.makeVisible();
+   }
+   
+   /**
+    * metodo que acomoda las figuras dibujadas y da forma a una casa.
+    * @param desplazar permite contener un valor para poder mover la casa
+    */
+   public void acomodate(int desplazar)
+   {   
+        ventana.moveHorizontal(30+desplazar);
+        ventana.moveDown();
+        ventana.moveVertical(-28);
     
-    techo.moveHorizontal(15+desplazar);
-    techo.changeSize(87,87);
-    techo.moveVertical(-16);
-    
-    ventana.makeVisible();
-      
-    pared.changeSize(80);
-    pared.moveDown();
-    pared.moveDown();
-    pared.moveHorizontal(-35+desplazar);
-    pared.moveVertical(-5);
-    
-    puerta.changeColor("black");
-    puerta.moveVertical(85);
-    puerta.moveHorizontal(10+desplazar);
-           
-    
-}
+        techo.moveHorizontal(15+desplazar);
+        techo.changeSize(87,87);
+        techo.moveVertical(-16);
+        
+        ventana.makeVisible();
+          
+        pared.changeSize(80);
+        pared.moveDown();
+        pared.moveDown();
+        pared.moveHorizontal(-35+desplazar);
+        pared.moveVertical(-5);
+        
+        puerta.changeColor("black");
+        puerta.moveVertical(85);
+        puerta.moveHorizontal(10+desplazar);
+   }
 
-void casa2(int y)
-{
-    casa2 = new Casa();
+   /**
+    * metodo que dibuja otra casa.
+    * @param y permite terner un desplazamiento para que la otra cas de mueva y quede 
+    * a un lado de la primer casa.
+    */
+   public void casa2(int y)
+   {
+       casa2 = new Casa();
     
-    casa2.dibujate();
-    casa2.acomodate(y);
-}
-
-
+       casa2.dibujate();
+       casa2.acomodate(y);
+   }
 }
